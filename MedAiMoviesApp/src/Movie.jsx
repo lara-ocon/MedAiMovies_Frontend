fb741a487b0ed9f41d2d2939344a0cd2091a1c2f
Miguel Ara Adánez <202102043@alu.comillas.edu>
2024-05-02 23:27:26 +0200

    [STYLE] - Fixed style for movie distribution

--- MedAiMoviesApp/src/Movie.jsx
+++ MedAiMoviesApp/src/Movie.jsx
@@ -6,8 +6,7 @@ function Movie({ movie }) {
     return (
         <Link to={`/movie/${movie.id}`} className="movie">
             <img src={movie.poster} alt={movie.titulo} />
-            <h3>{movie.titulo}</h3>
-            <p>{movie.fecha_estreno.split('-')[0]}</p>
+            <h3>{movie.titulo}<br />{movie.fecha_estreno.split('-')[0]}</h3>
         </Link>
     );
 }
