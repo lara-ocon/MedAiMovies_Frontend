ffb73ac87c91a26d0b6840724f1153150862d1f0
lara-ocon <202115710@alu.comillas.edu>
2024-05-03 13:12:25 +0200

    [STYLE] - Changed some styles

--- MedAiMoviesApp/src/Movie.jsx
+++ MedAiMoviesApp/src/Movie.jsx
@@ -6,7 +6,10 @@ function Movie({ movie }) {
     return (
         <Link to={`/movie/${movie.id}`} className="movie">
             <img src={movie.poster} alt={movie.titulo} />
-            <h3>{movie.titulo}<br />{movie.fecha_estreno.split('-')[0]}</h3>
+            <h3>{movie.titulo}</h3>
+            <h4>{movie.fecha_estreno.split('-')[0]}</h4>
+            {/* Ponemos parte de la sinopsis para que no ocupe tanto espacio en la lista de películas */}
+            {/*<p>{movie.sinopsis.slice(0, 110)}...</p>*/}
         </Link>
     );
 }
